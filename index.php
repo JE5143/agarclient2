8482857d5c2449b3a00703eba362f95941621e13
JE5143 <48130949+JE5143@users.noreply.github.com>
2019-05-06 09:09:40 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 <?php header( 'Location: /index.html' ) ;  ?>
+< ?php
+
+function redirectTohttps() {
+
+if($_SERVER[‘HTTPS’]!=”on”) {
+
+$redirect= “https://”.$_SERVER[‘HTTP_HOST’].$_SERVER[‘REQUEST_URI’];
+
+header(“Location:$redirect”); } }
+
+?>
+ 
